46ea9665abcaa6a6d5fc7e1df6f71c327059624d
Nourchen05 <noamennourchen5@gmail.com>
2021-11-19 23:16:53 +0100

    cookie part

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,24 @@
 const express = require("express");
 const app = express();
+const cookieSession = require("cookie-session");
+const passport = require("passport");
+
+app.use(
+  cookieSession({
+    name: "session",
+    keys: ["auth"],
+    maxAge: 24 * 60 * 60 * 100,
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(cors({
+    origin:"http://localhost:3000",
+    method="GET,POST,PUT,DELETE",
+    credentials:true,
+}))
 
 app.listen("5000", () => {
   console.log("server is running");
