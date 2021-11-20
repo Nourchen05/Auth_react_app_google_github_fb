d70c4857b36a849461b48b2274c9d678a2257cea
Nourchen05 <noamennourchen5@gmail.com>
2021-11-20 21:54:00 +0100

    auth.js configuration

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,11 +3,14 @@ const passport = require("passport");
 
 const CLEINT_URL = "http://localhost:3000/";
 
-router.get("/login/failed", (req, res) => {
-  res.status(401).json({
-    success: false,
-    message: "failure",
-  });
+router.get("/login/success", (req, res) => {
+  if (req.user) {
+    res.status(200).json({
+      success: true,
+      message: "successfull",
+      user: req.user,
+    });
+  }
 });
 
 router.get("/login/failed", (req, res) => {
@@ -15,6 +18,11 @@ router.get("/login/failed", (req, res) => {
     success: false,
     message: "failure",
   });
+});
+
+router.get("/logout", (req, res) => {
+  req.logout();
+  res.redirect(CLIENT_URL);
 });
 
 router.get("/google", passport.authenticate("google", { scope: ["profile"] }));
